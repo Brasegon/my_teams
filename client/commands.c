0a9c11380ed6448d8868ad31cadbcc3e8eecce7a
brandon.segers@epitech.eu <brandon.segers@epitech.eu>
2020-05-25 20:11:30 +0200

    suppression de norme

--- client/commands.c
+++ client/commands.c
@@ -6,19 +6,6 @@
 */
 
 #include "include/client.h"
-// #include "include/commands.h"
-
-const t_commands commands[] =
-{
-    {"/login", &login},
-    {"/logout", &logout},
-    {NULL, NULL}
-};
-
-void sendMessage(int fd, char *message)
-{
-    dprintf(fd, "%s", message);
-}
 
 int countTab(char **tab)
 {
@@ -26,14 +13,4 @@ int countTab(char **tab)
     while (tab[i])
         i++;
     return (i);
-}
-
-void launchCommand(char **tab, t_client *c)
-{
-    for (int i = 0; commands[i].command != NULL; i++) {
-        if (strcmp(tab[0], commands[i].command) == 0) {
-            (commands[i].func)(tab, c->fd);
-            return;
-        }
-    }
 }
